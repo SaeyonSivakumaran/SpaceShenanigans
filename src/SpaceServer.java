471864be84776f97778a900d69a2c5eeddfef67b
SaeyonSivakumaran <saeyon2000@gmail.com>
2018-01-04 13:25:09 -0500

    Updated server

--- src/SpaceServer.java
+++ src/SpaceServer.java
@@ -13,6 +13,14 @@ import javax.swing.JTextArea;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+//Imports for buttons
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+//Networking imports
+import java.net.Socket;
+import java.net.ServerSocket;
+
 class SpaceServer extends JFrame{
 	
 	//GUI Variables
@@ -21,6 +29,10 @@ class SpaceServer extends JFrame{
 	private JTextArea consoleOutput;
 	private JButton startButton;
 	private JButton endButton;
+	
+	//Networking variables
+	private boolean running = true;
+	private ServerSocket serverSocket;
 	
 	/**
 	 * Constructor for SpaceServer
@@ -35,7 +47,9 @@ class SpaceServer extends JFrame{
 		consoleOutput = new JTextArea();
 		consoleOutput.setEditable(false);
 		startButton = new JButton("Start Server");
+		startButton.addActionListener(new startButtonListener());
 		endButton = new JButton("Stop Server");
+		endButton.addActionListener(new endButtonListener());
 		//Adding buttons to the button panel
 		buttonPanel.add(startButton);
 		buttonPanel.add(endButton);
@@ -45,6 +59,46 @@ class SpaceServer extends JFrame{
 		//Completing the main frame
 		this.add(mainPanel);
 		this.setVisible(true);
+	}
+	
+	/**
+	 * Listener for the start button
+	 */
+	class startButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Server starting");
+			start();  //Starting the server
+		}
+	}
+	
+	/**
+	 * Listener for the end button
+	 */
+	class endButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Server stopping");
+			running = false;
+		}
+	}
+	
+	/**
+	 * Method to start server
+	 * @param Nothing
+	 * @return Nothing
+	 */
+	public void start() {
+		System.out.println("Server started");
+		Socket client = null;  //Socket for client
+		//Waiting for connection
+		try {
+			serverSocket = new ServerSocket(5000);
+			while (running) {
+				client = serverSocket.accept();
+				System.out.println("Client connected");
+			}
+		} catch(Exception e) {
+			System.out.println("Connection failed");
+		}
 	}
 	
 }
