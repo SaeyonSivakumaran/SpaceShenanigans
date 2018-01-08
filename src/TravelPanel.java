2dfb9cc4a6d919b835a4938666dadf868434bae2
AsianFruit <33629388+AsianFruit@users.noreply.github.com>
2018-01-08 15:26:09 -0500

    Scrolly thing not as scrolly

--- src/TravelPanel.java
+++ src/TravelPanel.java
@@ -5,10 +5,12 @@ import java.io.*;
 
 public class TravelPanel extends JPanel {
 	Image backgroundIcon = null;
+	Image shipImage = null;
 	int x = 0;
 	public TravelPanel(){
 		try { //loading images
 			backgroundIcon = new ImageIcon("SpaceBackground.png").getImage();
+			shipImage = new ImageIcon("EngineModule4.png").getImage();
 		} catch (Exception ex) {
 			System.out.println("image didn't load");
 		}
@@ -17,10 +19,11 @@ public class TravelPanel extends JPanel {
 		super.paintComponent(g); // required to ensure the panel is correctly redrawn
 		g.drawImage(backgroundIcon, 0, 0, 750 - x, 300, x, 0, 750, 300, null); //draw the part of the image from the left
 		g.drawImage(backgroundIcon, 750 - x, 0, 750, 300, 0, 0, x, 300, null); //draw the rest of the image
+		g.drawImage(shipImage, 200, 50, null);
 		if(x < 750) {
-			x += 5; //continue moving to the right
+			x += 3; //continue scrolling from left to right
 		}else {
-			x = 0;
+			x = 0; //right image has completely covered, reset to continue the loop
 		}
 		// request a repaint
 		repaint();
