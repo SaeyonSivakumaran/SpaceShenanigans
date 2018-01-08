9c8c5083e1386b60d20424038b2de94be057d014
ey lamo <34716276+eylamo@users.noreply.github.com>
2018-01-08 10:07:50 -0500

    oh yeaaaaf
    u ghey ah

---
+++ src/Planet.java
@@ -0,0 +1,15 @@
+
+public class Planet {
+	String name;
+	String resource;
+	int resourceAmount;
+	Player player1, player2;
+	Boolean travel;
+	int updateTime;
+	static final String ALPHABET="abcdefghijklmnopqrstuvwxyz";
+	
+	public Planet(){
+		
+	}
+	
+}
