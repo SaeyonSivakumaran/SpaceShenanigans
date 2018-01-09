026aa7919bd33a2e2655037223d4a04f0b18b0e3
AsianFruit <33629388+AsianFruit@users.noreply.github.com>
2018-01-09 11:38:06 -0500

    Adding Resizing to upgrade panel

--- src/TravelPanel.java
+++ src/TravelPanel.java
@@ -4,27 +4,31 @@ import java.awt.*;
 import java.io.*;
 
 public class TravelPanel extends JPanel {
-	Image backgroundIcon = null;
+	Image backgroundImage = null;
 	Image shipImage = null;
 	Clock clock = new Clock();
 	int x = 0;
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	int screenX = (int)screenSize.getWidth();
+	int screenY = (int)screenSize.getHeight();
+	
 	public TravelPanel(){
 		try { //loading images
-			backgroundIcon = new ImageIcon("SpaceBackground.png").getImage();
+			backgroundImage = new ImageIcon("SpaceBackground.png").getImage();
 			shipImage = new ImageIcon("ShieldSupercharger.png").getImage();
 		} catch (Exception ex) {
 			System.out.println("image didn't load");
 		}
+		backgroundImage = backgroundImage.getScaledInstance(screenX, screenY, Image.SCALE_DEFAULT); //resize background image
 	}
 	public void paintComponent(Graphics g) {
 		clock.update();
-		System.out.println(clock.getElapsedTime());
 		super.paintComponent(g); // required to ensure the panel is correctly redrawn
-		g.drawImage(backgroundIcon, 0, 0, 750 - x, 300, x, 0, 750, 300, null); //draw the part of the image from the left
-		g.drawImage(backgroundIcon, 750 - x, 0, 750, 300, 0, 0, x, 300, null); //draw the rest of the image
+		g.drawImage(backgroundImage, 0, 0, screenX - x, screenY, x, 0, screenX, screenY, null); //draw the part of the image from the left
+		g.drawImage(backgroundImage, screenX - x, 0, screenX, screenY, 0, 0, x, screenY, null); //draw the rest of the image
 		g.drawImage(shipImage, 200, 50, null);
-		if(x < 750) {
-			x = (int) (x + clock.getElapsedTime()*1000); //continue scrolling from left to right
+		if(x < screenX) {
+			x = (int) (x + clock.getElapsedTime()*1500); //continue scrolling from left to right
 		}else {
 			x = 0; //right image has completely covered, reset to continue the loop
 		}
@@ -33,15 +37,15 @@ public class TravelPanel extends JPanel {
 	}
 	
 	class Clock {
-		 long elapsedTime; //time that has passed
-		 long lastTimeCheck; //when the last time check occured
+		 double elapsedTime; //time that has passed
+		 double lastTimeCheck; //when the last time check occured
 		 public Clock() { //constructor starts with the first time check
 		 lastTimeCheck=System.nanoTime();
 		 elapsedTime=0;
 		 }
 
 		 public void update() { //called each loop of the game loop
-		 long currentTime = System.nanoTime(); //if the computer is fast you need more precision
+		 double currentTime = System.nanoTime(); //if the computer is fast you need more precision
 		 elapsedTime=currentTime - lastTimeCheck; //update current time
 		 lastTimeCheck=currentTime; //update last time check
 		 }
@@ -56,6 +60,7 @@ public class TravelPanel extends JPanel {
 		JFrame frame = new JFrame("hello");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(new TravelPanel());
+        frame.setPreferredSize(new Dimension(1920,1080));
         frame.pack();
 		frame.setVisible(true);
 	}
