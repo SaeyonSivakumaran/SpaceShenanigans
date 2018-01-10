8d842baa7638c09c8691870e5a97025a675090b7
eylamo <mk.z2009@hotmail.com>
2018-01-10 10:00:01 -0500

    u homosexuel

--- src/Planet.java
+++ src/Planet.java
@@ -27,7 +27,7 @@ public class Planet {
 		break;
 		case 1: resource="Steel";
 		resourceAmount=(int) (Math.random()*100+50);
-		resourceRate=10;
+		resourceRate=20;
 		mineRate=200;
 		break;
 		case 2: resource="Intellectium";
