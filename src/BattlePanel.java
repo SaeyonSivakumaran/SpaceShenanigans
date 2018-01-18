1410a396436eff6dca4680cb209f64f226da43d3
AsianFruit <33629388+AsianFruit@users.noreply.github.com>
2018-01-17 22:54:42 -0500

    Gameframe gone for now

--- src/BattlePanel.java
+++ src/BattlePanel.java
@@ -1,5 +1,6 @@
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.image.*;
 import java.awt.event.*;
@@ -9,14 +10,20 @@ import javax.imageio.*;
 
 public class BattlePanel extends JPanel {
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	static JFrame frame = new JFrame();
 	int screenX = (int) screenSize.getWidth();
 	int screenY = (int) screenSize.getHeight();
 	Image backgroundImage = null;
-
+	Color clearRed = new Color(247, 9, 49, 150); // create 50% transparent colours
+	Color clearGreen = new Color(16, 196, 9, 200);
 	BufferedImage backgroundPlanet = null;
+	BufferedImage userShip;
+	BufferedImage opponentShip;
+	JButton button1;
+	JButton button2;
+	String userName;
+	String opponentName;
 	
-	public BattlePanel(String planetName) {
+	public BattlePanel(String planetName, BufferedImage userShip, BufferedImage opponentShip, String weapon1, String weapon2, String userName, String opponentName) {
 		try { // loading images
 			backgroundImage = new ImageIcon("SpaceMap.png").getImage();
 			if(planetName.equals("Yarn Planet")) {
@@ -33,14 +40,94 @@ public class BattlePanel extends JPanel {
 				backgroundPlanet = ImageIO.read(new File("jupiter.png"));
 			}else if (planetName.equals("Moon Planet")){
 				backgroundPlanet = ImageIO.read(new File("moonPlanet.png"));
+			}else if (planetName.equals("Basketball Planet")) {
+				backgroundPlanet = ImageIO.read(new File("basketball.png"));
+			}else if (planetName.equals("Bouncy Planet")) {
+				backgroundPlanet = ImageIO.read(new File("bouncyPlanet.png"));
+			}else if (planetName.equals("Saturn Planet")) {
+				backgroundPlanet = ImageIO.read(new File("saturnPlanet.png"));
 			}
 		} catch (Exception ex) {
 			System.out.println("image didn't load");
 		}
+		this.userShip = resizeImage(userShip, screenX/4, (screenX/4)/3);
+		this.opponentShip = resizeImage(opponentShip, screenX/4, (screenX/4)/3);
+		this.userName = userName;
+		this.opponentName = opponentName;
 		
 		backgroundImage = backgroundImage.getScaledInstance(screenX, screenY, Image.SCALE_DEFAULT);
-		backgroundPlanet = resizeImage(backgroundPlanet, screenX / 4, screenX / 4);
+		backgroundPlanet = resizeImage(backgroundPlanet, screenX / 3, screenX / 3);
+		
+		this.setLayout(null);
+		
+		//Create Panel with buttons to select attack
+		JPanel attackPanel = new JPanel();
+		attackPanel.setLayout(new BorderLayout());
+		attackPanel.setPreferredSize(new Dimension(screenX/2, screenY/3));
+		attackPanel.setBounds(0, 2*screenY/3, screenX/2, screenY);
+		attackPanel.setBackground(clearGreen);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.setBackground(clearGreen);
+		
+		JLabel attackLabel = new JLabel("Attack!", SwingConstants.CENTER);
+		attackLabel.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		
+		button1 = new JButton("Use " + weapon1);
+		button1.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		button1.addActionListener(new AttackButtonListener());
+		
+		button2 = new JButton("Use " + weapon2);
+		button2.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		button2.addActionListener(new AttackButtonListener());
+		
+		buttonPanel.add(button1);
+		buttonPanel.add(button2);
+		attackPanel.add(attackLabel, BorderLayout.NORTH);
+		attackPanel.add(buttonPanel, BorderLayout.CENTER);
+		
+		//Create Panel with text for when it's not this player's turn
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new BorderLayout());
+		textPanel.setPreferredSize(new Dimension(screenX/2, screenY/3));
+		textPanel.setBounds(0, 2*screenY/3, screenX/2, screenY);
+		textPanel.setBackground(clearGreen);
+		
+		JLabel messageLabel;
+		if (true) { //messages to be displayed when your attack has hit
+			messageLabel = new JLabel("Your Laser has hit " + opponentName + "!", SwingConstants.CENTER);
+		} else if (true) {
+			messageLabel = new JLabel("Your ShieldJammer has hit " + opponentName + "!", SwingConstants.CENTER);
+		} else if (true) {
+			messageLabel = new JLabel("Your Missle has hit " + opponentName + "!", SwingConstants.CENTER);
+		} else if (true) {
+			messageLabel = new JLabel("Your attack has missed!", SwingConstants.CENTER);
+		}else if (true) {
+			messageLabel = new JLabel(opponentName + "'s ShieldJammer has hit!", SwingConstants.CENTER);
+		}else if (true) {
+			messageLabel = new JLabel(opponentName + "'s Laser has hit!", SwingConstants.CENTER);
+		}else if (true) {
+			messageLabel = new JLabel(opponentName + "'s Missile has hit!", SwingConstants.CENTER);
+		}else if (true) {
+			messageLabel = new JLabel(opponentName + "'s ShieldJammer has hit!", SwingConstants.CENTER);
+		}else if (true) {
+			messageLabel = new JLabel(opponentName + "'s attack has missed!", SwingConstants.CENTER);
+		}
+		
+		this.add(attackPanel);
 	}
+	
+	public class AttackButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == button1){
+	
+			}else if (e.getSource() == button2) {
+				
+			}
+		}
+	}
+	
 	
 	/*
 	 * resizeImage 
@@ -57,26 +144,66 @@ public class BattlePanel extends JPanel {
 		Graphics2D g2d = originalImg.createGraphics();
 		g2d.drawImage(temp, 0, 0, null);
 		g2d.dispose();
-
+		
 		return originalImg;
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g); // required to ensure the panel is correctly redrawn
 		g.drawImage(backgroundImage, 0, 0, null);
-		g.drawImage()
-		
+		g.drawImage(backgroundPlanet, screenX / 20, screenX / 20, null);
+
+		// Draw translucent rectangle
+		g.setColor(clearRed);
+		g.fillRect(screenX/2, 0, screenX/2, screenY);
+		g.drawImage(userShip, 0, 0, null);
+		g.drawImage(opponentShip, screenX - opponentShip.getWidth(), 0, null);
 		repaint();
 	}
 	
+	public static BufferedImage combineSpaceship(BufferedImage ship, BufferedImage engine, BufferedImage weapon1, BufferedImage weapon2) {
+		BufferedImage builtShip = new BufferedImage(900, 300, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = builtShip.getGraphics();
+		g.drawImage(ship, 0, 0, null);
+		g.drawImage(engine, 0, 0, null);
+		g.drawImage(weapon1, 600, 150, null);
+		g.drawImage(weapon2, 600, 200, null);
+		return builtShip;
+	}
+	
 	public static void main(String args[]) {
+		BufferedImage ship = null;
+		BufferedImage laser = null;
+		BufferedImage shieldJammer = null;
+		BufferedImage missile = null;
+		BufferedImage engine1 = null;
+		BufferedImage engine2 = null;
+		BufferedImage engine3 = null;
+		BufferedImage engine4 = null;
+		BufferedImage engine5 = null;
+
+		try {
+			ship = ImageIO.read(new File("SpaceShip.png"));
+			
+			engine1 = ImageIO.read(new File("EngineModule1.png"));
+			engine2 = ImageIO.read(new File("EngineModule2.png"));
+			engine3 = ImageIO.read(new File("EngineModule3.png"));
+			engine4 = ImageIO.read(new File("EngineModule4.png"));
+			engine5 = ImageIO.read(new File("EngineModule5.png"));
+
+			laser = ImageIO.read(new File("Laser.png"));
+			missile = ImageIO.read(new File("MissileLauncher.png"));
+			shieldJammer = ImageIO.read(new File("ShieldJammer.png"));
+		}catch(Exception e) {
+			System.out.println("Failed to load images from main");
+		}
+		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(new MapPanel());
+		frame.setContentPane(new BattlePanel("Jupiter Planet", combineSpaceship(ship, engine4, laser, shieldJammer), combineSpaceship(ship, engine2, laser, missile), "Laser", "ShieldJammer", "noob_1", "noob_2"));
 		frame.setPreferredSize(new Dimension((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
 				(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight()));
 		frame.pack();
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
-
 }
